a5aa4510a2bbff43e181182f761a1b333af8a39c
Dusty Miller <dusty.miller109@gmail.com>
2023-11-02 20:17:32 -0400

    Part 1 day 3

---
+++ 3.py
@@ -0,0 +1,35 @@
+def part_one(input):
+    total = 0
+    for line in input.split("\n"):
+        ic = int(len(line)/2)
+        c1 = line[:ic]
+        c2 = line[ic:]
+        match = ord(min(set(c1).intersection(c2)))
+        if match > 97:
+            match = match - 96
+        else:
+            match = match - 38
+        total += match
+    print(total)
+
+
+
+def part_two(input):
+    pass
+
+def main():
+    with open("input_3") as file:
+        input = file.read()
+    test_input = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+    part_one(input)
+    part_two(input)
+
+
+
+if __name__ == "__main__":
+    main()
